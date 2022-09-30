94857034a9d54a4026b4d28bd306bce9279ec048
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-09-29 20:59:38 -0500

    Ajustes

---
+++ src/util/FechaUtil.java
@@ -0,0 +1,80 @@
+package util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class FechaUtil {
+
+	public static String getFechaActualYYYYMMdd() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date fechaActal = new Date();
+		return sdf.format(fechaActal);
+	}
+	
+	public static String getFechaActualddMMyyyy() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Date fechaActal = new Date();
+		return sdf.format(fechaActal);
+	}
+	
+	public static String getFechaPrimeroEneroYYYYMMdd() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		//El calendar permite operar con fechas
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, 1);  //1 en el mes
+		calendar.set(Calendar.MONTH, Calendar.JANUARY); //El mes de Enero
+		Date fecha = calendar.getTime();
+		
+		//Le damos el formato
+		return sdf.format(fecha);
+	}
+	
+	public static String getFechaUltimoDiciembreYYYYMMdd() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		//El calendar permite operar con fechas
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, 31);  //31 en el mes
+		calendar.set(Calendar.MONTH, Calendar.DECEMBER); //El mes de Diciembre
+		Date fecha = calendar.getTime();
+		
+		//Le damos el formato
+		return sdf.format(fecha);
+	}
+	
+	public static boolean isSuperiorFechaYYYYMMdd(String fecUno, String fecDos) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date dateUno = sdf.parse(fecUno);
+			Date dateDos = sdf.parse(fecDos);
+			return dateDos.after(dateUno);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean isSuperiorSeisMesesFechaYYYYMMdd(String fecUno, String fecDos) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date dateUno = sdf.parse(fecUno);
+			Date dateDos = sdf.parse(fecDos);
+			
+			//Aumentar la fecha 1 en seis meses (180 días)
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(dateUno);
+			int diasAnio = calendar.get(Calendar.DAY_OF_YEAR);
+			calendar.set(Calendar.DAY_OF_YEAR, diasAnio + 180);
+			Date  dateAumentado = calendar.getTime();
+			
+			System.out.println(sdf.format(dateAumentado));
+			return dateAumentado.after(dateDos);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+}
