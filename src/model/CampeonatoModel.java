baf8d32d23405f0cbe600d034a85fc78842fb5fd
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-10-01 07:03:34 -0500

    Ajustes

--- src/model/CampeonatoModel.java
+++ src/model/CampeonatoModel.java
@@ -2,9 +2,6 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import entidad.Campeonato;
@@ -48,39 +45,6 @@ public class CampeonatoModel {
 		return salida;
 	}
 
-	public List<Campeonato> listaPorNombre(String filtro) {
-		List<Campeonato> lista = new ArrayList<Campeonato>();
-		Connection conn = null;
-		PreparedStatement pstm = null;
-		ResultSet rs = null;
-		try {
-			conn = MySqlDBConexion.getConexion();
-			String sql = "select * from campeonato where nombre like ?";
-			pstm = conn.prepareStatement(sql);
-			pstm.setString(1, filtro + "%");
-			log.info(">>> " + pstm);
-			rs = pstm.executeQuery();
-			Campeonato objCampeonato = null;
-			while (rs.next()) {
-				objCampeonato = new Campeonato();
-				objCampeonato.setIdCampeonato(rs.getInt(1));
-				objCampeonato.setNombre(rs.getString(2));
-				objCampeonato.setAnnio(rs.getInt(3));
-				objCampeonato.setFechaRegistro(rs.getDate(4));
-				objCampeonato.setEstado(rs.getInt(5));
-				lista.add(objCampeonato);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)		rs.close();
-				if (pstm != null)	pstm.close();
-				if (conn != null)	conn.close();
-			} catch (Exception e2) {
-			}
-		}
-		return lista;
-	}
+	
 
 }
