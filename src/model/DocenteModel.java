f5232aa33ddf465fc5f04a5d83e69099d20e6255
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-10-01 10:37:28 -0500

    Cambios

--- src/model/DocenteModel.java
+++ src/model/DocenteModel.java
@@ -1,11 +1,40 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import entidad.Docente;
 
 public class DocenteModel {
 
 	private static Logger log = Logger.getLogger(DocenteModel.class.getName());
 	
+	public List<Docente> listaDocentePorFecha(Date fecIni, Date fecFin){
+		List<Docente> salida = new ArrayList<Docente>();
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		try {
+
+			log.info("SQL >> " + pstm);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstm != null) pstm.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {}
+		}
+		
+		return salida;
+	}
 	
 }
 
